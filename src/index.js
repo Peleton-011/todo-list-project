cd31051eb8119cc00fe597763754e017364321aa
Nico <nicodiazan@gmail.com>
2023-04-25 20:27:46 +0200

    Implement a tasklist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import "@picocss/pico";
 
 import getTaskForm from "./components/taskForm";
+import Task from "./logic/task";
 
+const taskList = [];
 const component = () => {
 
-    const taskOnSubmit = (e) => {
-        e.preventDefault();
-        console.log("test")
-    }
+	const taskOnSubmit = (e) => {
+		e.preventDefault();
+		const FD = new FormData(document.querySelector("form"));
+		const [taskTitle, description, dueDate, dueTime, priorityLevel] = [
+			FD.get("taskTitle"),
+			FD.get("description"),
+			FD.get("dueDate"),
+			FD.get("dueTime"),
+			FD.get("priorityLevel"),
+		];
+
+		taskList.push(
+			new Task({
+				title: taskTitle,
+				description: description,
+				priority: priorityLevel,
+				dueDate: [dueDate, dueTime],
+				id: 1,
+			})
+		);
+
+        console.log(taskList);
+	};
 	const component = getTaskForm(taskOnSubmit);
 
 	console.log(component);
