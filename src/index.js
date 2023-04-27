6be4890fb18198ce444767d2a1d8fbc7816cc492
Nico <nicodiazan@gmail.com>
2023-04-27 21:20:39 +0200

    Something something and such or whatever
    still got to make it so you can add tasks as children to projects

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import Project from "./logic/project";
 
 import Header from "./components/Header";
 
-const taskList = [];
-
 function tasksDisplayElem() {
 	const taskListElem = document.createElement("section");
 	taskListElem.id = "taskList";
@@ -16,70 +14,23 @@ function tasksDisplayElem() {
 	return taskListElem;
 }
 
-function removeTask(id) {
-	taskList.filter((currTask) => currTask.id !== id);
-	document.getElementById(id).remove();
-}
-
-function addTask({ taskTitle, description, dueDate, dueTime, priorityLevel }) {
-	const taskInterface = {
-		title: taskTitle,
-		description: description,
-		priority: priorityLevel,
-		dueDate: [dueDate, dueTime],
-	};
-	const newTask = new Task(taskInterface);
-	taskList.push(newTask);
-	const taskListElem = document.getElementById("taskList");
-
-	taskListElem.appendChild(newTask.getElem({onDel: removeTask}));
-}
-function addProject({
-	projectTitle,
-	description,
-	dueDate,
-	dueTime,
-	priorityLevel,
-}) {
-	const projectInterface = {
-		title: projectTitle,
-		description: description,
-		priority: priorityLevel,
-		dueDate: [dueDate, dueTime],
-	};
-	const newProject = new Project(projectInterface);
-	taskList.push(newProject);
-	const taskListElem = document.getElementById("taskList");
-
-	taskListElem.appendChild(newProject.getElem({onDel: removeTask}));
-}
-
-
-function addTasksTo({ tasks, target }) {
-	tasks
-		.filter((task) => task.type === "task")
-		.map((task) => task.getTaskElem({onDel: removeTask}))
-		.reduce((acc, task) => {
-			acc.appendChild(task);
-			return acc;
-		}, target);
-}
-
 const component = () => {
+	const mainProject = new Project({ title: "main" });
+
 	const component = document.createElement("main");
 	component.classList.add("container");
 
 	//Forms
-	const [ taskForm, toggleTaskForm]  = initializeForm({
+	const [taskForm, toggleTaskForm] = initializeForm({
 		type: "task",
-		addFunction: addTask,
+		addFunction: mainProject.addTask,
 		titlePlaceholder: "Get eggs for an omelette",
 		descriptionPlaceholder:
 			"Ask Danny if he has some, or go to the store to get them.",
 	});
-	const [ projectForm, toggleProjectForm ] = initializeForm({
+	const [projectForm, toggleProjectForm] = initializeForm({
 		type: "project",
-		addFunction: addProject,
+		addFunction: mainProject.addProject,
 		titlePlaceholder: "Learn how to cook",
 		descriptionPlaceholder:
 			"Start by learning at least 5 different recipes to mix and match",
@@ -95,7 +46,9 @@ const component = () => {
 
 	const taskListElem = tasksDisplayElem();
 
-	addTasksTo({ tasks: taskList, target: taskListElem });
+	// addTasksTo({ tasks: taskList, target: taskListElem });
+
+	taskListElem.appendChild(mainProject.getElem());
 
 	component.appendChild(taskListElem);
 
@@ -103,10 +56,3 @@ const component = () => {
 };
 
 document.body.appendChild(component());
-
-//FOR TESTING vvvv
-
-addTask({ taskTitle: "a", description: "b" });
-addTask({ taskTitle: "c", description: "d" });
-
-//FOR TESTING ^^^^
--- src/logic/project.js
+++ src/logic/project.js
@@ -1,5 +1,7 @@
 import Task from "./task";
 
+import initializeForm from "../components/Form";
+
 class Project extends Task {
 	type = "project";
 
@@ -7,17 +9,131 @@ class Project extends Task {
 		super(params);
 
 		this.taskList = [];
+		this.addTask = this.addTask.bind(this);
+		this.addProject = this.addProject.bind(this);
 	}
 
-	addTask(taskParams) {
-		this.taskList.push(new Task(taskParams));
+	removeTask(id) {
+		this.taskList.filter((currTask) => currTask.id !== id);
+		document.getElementById(id).remove();
 	}
 
-	getElem({ onDel }) {
-		const taskElem = super.getElem({ onDel });
+	addTask({ taskTitle, description, dueDate, dueTime, priorityLevel }) {
+		const taskInterface = {
+			title: taskTitle,
+			description: description,
+			priority: priorityLevel,
+			dueDate: [dueDate, dueTime],
+		};
+		const newTask = new Task(taskInterface);
+		this.taskList.push(newTask);
+		const taskListElem = document.getElementById("taskList");
+
+		taskListElem.appendChild(newTask.getElem({ onDel: this.removeTask }));
+	}
+
+	addProject({ projectTitle, description, dueDate, dueTime, priorityLevel }) {
+		const projectInterface = {
+			title: projectTitle,
+			description: description,
+			priority: priorityLevel,
+			dueDate: [dueDate, dueTime],
+		};
+		const newProject = new Project(projectInterface);
+		this.taskList.push(newProject);
+		const taskListElem = document.getElementById("taskList");
+
+		taskListElem.appendChild(
+			newProject.getElem({ onDel: this.removeTask })
+		);
+	}
+
+	getElem() {
+		const taskElem = super.getElem({ onDel: this.removeTask });
+
+		const content = taskElem.querySelector("details > p");
+
+		//Inner tasks and such
+
+		this.taskList
+			.map((task) => task.getElem({ onDel: this.removeTask }))
+			.reduce((acc, task) => {
+				acc.appendChild(task);
+				return acc;
+			}, content);
+
+		//Buttons
+
+		const btns = document.createElement("div");
+
+		const onAddTask = (e) => {
+			const [taskForm, toggleTaskForm] = initializeForm({
+				type: "task",
+				addFunction: this.addTask,
+				titlePlaceholder: "Get eggs for an omelette",
+				descriptionPlaceholder:
+					"Ask Danny if he has some, or go to the store to get them.",
+			});
+			document.querySelector("main").appendChild(taskForm);
+			toggleTaskForm(e);
+		};
+
+		const onAddProject = (e) => {
+			const [projectForm, toggleProjectForm] = initializeForm({
+				type: "project",
+				addFunction: this.addProject,
+				titlePlaceholder: "Learn how to cook",
+				descriptionPlaceholder:
+					"Start by learning at least 5 different recipes to mix and match",
+			});
+			document.querySelector("main").appendChild(projectForm);
+			toggleProjectForm(e);
+		};
+
+		const addTask = btn({ onclick: onAddTask, text: "Add Task" });
+
+		const addProject = btn({ onclick: onAddProject, text: "Add Project" });
+
+		btns.setAttribute(
+			"style",
+			`
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        justify-items: end;
+    
+        `
+		);
+
+		btns.appendChild(addTask);
+		btns.appendChild(addProject);
+
+		content.appendChild(btns);
+
+		// const btn = document.createElement("button");
+		// btn.innerText = "Add Task";
+		// btn.addEventListener("click", onClick);
+		// btn.setAttribute("role", "button");
+		// btn.setAttribute(
+		//     "style",
+		//     `width: fit-content;
+		// height: fit-content;`
+		// );
 
 		return taskElem;
 	}
+}
+
+function btn({ onclick, text }) {
+	const btn = document.createElement("button");
+	btn.innerText = text;
+	btn.addEventListener("click", onclick);
+	btn.setAttribute("role", "button");
+	btn.setAttribute(
+		"style",
+		`width: fit-content;
+    height: fit-content;`
+	);
+	return btn;
 }
 
 export default Project;
